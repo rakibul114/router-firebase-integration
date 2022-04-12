ba845b2f9524278c02297c01ef1cf3e9fc32c5f5
rakibul114 <rakibulislam114@gmail.com>
2022-04-12 15:47:37 +0600

    created all compenents and conneted to app.js file

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Register.css';
 
 const Register = () => {
     return (
+      <div className="register-container">
         <div>
-            <h1>Please register</h1>
+          <h1 className='form-title'>Please register</h1>
+          <form className="form-elements">
+            <input type="text" name="" id="" placeholder="Your Name" />
+            <br />
+            <input type="email" name="" id="" placeholder="Your Email" />
+            <br />
+                    <input type="password" name="" id="" />
+                    <br />
+                    <input type="submit" value="Register" />
+          </form>
         </div>
+      </div>
     );
 };
 
