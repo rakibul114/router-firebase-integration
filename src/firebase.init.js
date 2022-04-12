473debb6a0218f926dd3b85170e75038ba380525
rakibul114 <rakibulislam114@gmail.com>
2022-04-12 15:31:25 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2dTq5lYGHEkjqEkZy3PV3Cr7BKNuBcdg",
+  authDomain: "router-firebase-integrat-bc7c4.firebaseapp.com",
+  projectId: "router-firebase-integrat-bc7c4",
+  storageBucket: "router-firebase-integrat-bc7c4.appspot.com",
+  messagingSenderId: "942036248314",
+  appId: "1:942036248314:web:f74a643abee2d389074b43",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
